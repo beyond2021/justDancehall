e67e7ee419697efb53108d69b5ddd0db84a41420
Keevin Mitchell <keevin.mitchell@gmail.com>
2017-05-06 22:53:42 -0400

    Completed videoPlayerView adding the currentime label with functionality and slider thumb tracking

--- OnFleek/VideoLauncher.swift
+++ OnFleek/VideoLauncher.swift
@@ -10,26 +10,269 @@ import UIKit
 import AVFoundation
 //alot in here so make it a first class citezen
 class VideoPlayerView: UIView {
+    
+    var player : AVPlayer?
+    
+    let spinner : UIActivityIndicatorView = {
+        let sp = UIActivityIndicatorView()
+        sp.translatesAutoresizingMaskIntoConstraints = false
+        sp.startAnimating()
+        
+      return sp
+    }()
+    let controlsContainerView : UIView = {
+        let ccv = UIView()
+        //ccv.translatesAutoresizingMaskIntoConstraints = false //position not set with constraints
+        ccv.backgroundColor = UIColor(white: 0, alpha: 1.0)
+        
+        return ccv
+    }()
+    lazy var pausePlayButton:UIButton = {
+        let button = UIButton(type: .system)
+        //set imge on a uibuton
+        let image = UIImage(named: "Pause Filled-50")
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.isHidden = true
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    
+    let videoLengthLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "00:00"
+        label.textColor = .white
+        label.font = UIFont .boldSystemFont(ofSize: 13)
+        label.textAlignment = .right
+        
+      return label
+    }()
+    
+    let currentTimeLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "00:00"
+        label.textColor = .white
+        label.font = UIFont .boldSystemFont(ofSize: 13)
+        //label.textAlignment = .right
+        
+        return label
+    }()
+
+    
+    
+    
+   lazy var videoSlider : UISlider = {
+        let sl = UISlider()
+        sl.translatesAutoresizingMaskIntoConstraints = false
+        sl.minimumTrackTintColor = .red
+        sl.maximumTrackTintColor = .white
+        sl.setThumbImage(UIImage(named:"New Moon Filled-50"), for: .normal)
+    sl.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+     return sl
+    }()
+    
+    
+    func handleSliderChange(){
+       // print(videoSlider.value)
+        //SCRUBBER
+        if  let duration = player?.currentItem?.duration {
+            let totalSeconds = CMTimeGetSeconds(duration)
+            let value = Float64(videoSlider.value) * totalSeconds
+            
+            let seekTime = CMTime(value: Int64(value), timescale: 1)
+            player?.seek(to: seekTime, completionHandler: { (completionSeek) in
+                //
+            })
+        }
+        //
+        
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupPlayerView() // this view is now under the controls view
+        
+        //Adding a Gradient to this view.
+        setupGradientLayer()
+        
+        controlsContainerView.frame = frame ////position not set with constraints 
+        addSubview(controlsContainerView)
+        addSubview(spinner)
+        
+        spinner.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        //spinner.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        //spinner.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        //
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        //
+        controlsContainerView.addSubview(videoLengthLabel)
+        videoLengthLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        videoLengthLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -2).isActive = true
+        videoLengthLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        videoLengthLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        
+        
+        
+        controlsContainerView.addSubview(videoSlider)
+        videoSlider.rightAnchor.constraint(equalTo: videoLengthLabel.leftAnchor).isActive = true
+        videoSlider.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        videoSlider.widthAnchor.constraint(equalToConstant: self.frame.width - 136).isActive = true
+        videoSlider.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        
+        controlsContainerView.addSubview(currentTimeLabel)
+        currentTimeLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        currentTimeLabel.rightAnchor.constraint(equalTo: videoSlider.leftAnchor, constant: -8).isActive = true
+        currentTimeLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -2).isActive = true
+        currentTimeLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+
+        
+        
+        
+        
         backgroundColor = .black
         //"http://techslides.com/demos/sample-videos/small.mp4"
-        // Add the player
-        let urlString = "http://techslides.com/demos/sample-videos/small.mp4"
+        
+    }//
+    
+    
+    
+    private func setupPlayerView(){
+        
+        //https://dwknz3zfy9iu1.cloudfront.net/uscenes_h-264_hd_test.mp4
+        
+        let urlString = "https://dwknz3zfy9iu1.cloudfront.net/uscenes_h-264_hd_test.mp4"
+        
         if let url = NSURL(string: urlString){
-            let player = AVPlayer(url: url as URL)
+            player = AVPlayer(url: url as URL)
             // make the video render - show
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
-            player.play()
+            player?.play()
+            
+            //attach an observer to know when the player starts to play
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+            
+            
+            //Adding a progress monitor onto the playerview
+            //track player progress
+            let interval = CMTime(value: 1, timescale: 2)
+            player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { (progress) in
+                //progress time
+                let seconds = CMTimeGetSeconds(progress)
+                let arg = seconds.truncatingRemainder(dividingBy: 60)
+                let secondsString = String(format: "%02d", Int(arg))
+                let minutesString = String(format: "%02d", Int(seconds / 60))
+                self.currentTimeLabel.text = "\(minutesString):\(secondsString)"
+                
+                //MOVE THE SLIDER THUMB - GOES FROM 0 - 1 = PERCENTAGE OF WHAT IS PLAYED - currentPlayTime / duration
+                
+                if let duration = self.player?.currentItem?.duration {
+                    //CMTime
+                    let durationSeconds = CMTimeGetSeconds(duration)
+                    
+                    self.videoSlider.value = Float(seconds / durationSeconds)
+
+                }
+                
+                
+                
+            })
+            
         }
         
-    }//
+    }
+    
+    /*
+     /Users/kev1/Desktop/OnFleek/OnFleek/VideoLauncher.swift:173:34: '%' is unavailable: Use truncatingRemainder instead
+ 
+ */
+    //METHOD NEEDED FOR OBSERVER
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        //This is when the player is ready and redering frames
+        if keyPath == "currentItem.loadedTimeRanges" {
+            //print("Change is:", change ?? "")
+            spinner.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            // Play state
+            isPlaying = true
+            //
+            //readyForPlayback here
+            //LENGTH OF THE VIDEO
+            if  let duration = player?.currentItem?.duration {
+                let seconds = CMTimeGetSeconds(duration) //this is how long the track is.
+                
+                
+                let arg = seconds.truncatingRemainder(dividingBy: 60)
+                
+                
+                let secondsText = String(format: "%02d",Int( arg))
+                
+                let minutesText = String(format: "%02d", Int(seconds) / 60)
+                
+                videoLengthLabel.text = "\(minutesText):\(secondsText)"
+                
+            }
+            
+        }
+    }
+    var isPlaying = false //player State flag
+    
+    func handlePause(){
+        //print("Pausing player")
+        if isPlaying {
+            player?.pause()
+            //
+            // playButton.isHidden = true
+            let image = UIImage(named: "Play Filled-50")
+            pausePlayButton.setImage(image, for: .normal)
+        } else {
+            player?.play()
+        }
+        
+                      // Play state
+        isPlaying = !isPlaying
+        
+        
+    }
+    
+    //MARK:- Gradient
+    private func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds //Where the gradient layer starts and ends
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1.2]
+        controlsContainerView.layer.addSublayer(gradientLayer)
+        
+        
+    }
+
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    
+    
+    
+    
 }
 
 class VideoLauncher: NSObject {
